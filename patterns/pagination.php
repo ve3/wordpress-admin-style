495df490222e9b54b627bd0125da74b457ff96ff
ve3 <kawewong@gmail.com>
2025-12-28 11:49:05 +0700

    Update admin pagination example.
    Normalize quote attribute to use double quotes. Update HTML tags and classes to use currently of WordPress style.

--- patterns/pagination.php
+++ patterns/pagination.php
@@ -2,11 +2,17 @@
 
 <div class="tablenav">
 	<div class="tablenav-pages">
-		<span class="displaying-num"><?php _e( 'Example markup for <em>n</em> items', 'WpAdminStyle' ); ?></span>
-		<a class='first-page disabled' title='Go to first page' href='#'>&laquo;</a>
-		<a class='prev-page disabled' title='Go to previous page' href='#'>&lsaquo;</a>
-		<span class="paging-input"><input class='current-page' title='Current page' type='text' name='paged' value='1' size='1' /> of <span class='total-pages'>5</span></span>
-		<a class='next-page' title='Go to next page' href='#'>&rsaquo;</a>
-		<a class='last-page' title='Go to last page' href='#'>&raquo;</a>
+		<span class="displaying-num"><?php _e( 'Example markup for <em>n</em> items', 'WpAdminStyle' ); ?></span> 
+		<span class="pagination-links">
+			<span class="pagination-links"><span class="tablenav-pages-navspan button disabled" aria-hidden="true">&laquo;</span>
+			<span class="tablenav-pages-navspan button disabled" aria-hidden="true">&lsaquo;</span>
+			<span class="paging-input">
+				<label for="current-page-selector" class="screen-reader-text">Current Page</label>
+				<input id="current-page-selector" class="current-page" type="text" name="paged" value="1" size="1" aria-describedby="table-paging" />
+				<span class="tablenav-paging-text"> of <span class="total-pages">5</span></span>
+			</span>
+			<a class="next-page button" href="#"><span class="screen-reader-text">Next page</span><span aria-hidden="true">&rsaquo;</span></a>
+			<a class="last-page button" href="#"><span class="screen-reader-text">Last page</span><span aria-hidden="true">&raquo;</span></a>
+		</span>
 	</div>
 </div>
